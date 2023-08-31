07ccccc4d9570317eb7d3c2baafa16b744ab92d4
Muhammad Amray <muhammadamray@gmail.com>
2023-08-31 14:31:26 -0400

    root

---
+++ art-museum/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+function Root() {
+  return (
+    <App />
+    // <h1>Hello from Root</h1>
+  )
+
+
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
